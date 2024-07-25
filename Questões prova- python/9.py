d14629acfe6164460dc42ff0da0499de46e49224
Reh <robinhoitaimcoelho@gmail.com>
2024-07-25 16:54:13 -0300

    questoes respondidas

--- Questões prova- python/9.py
+++ Questões prova- python/9.py
@@ -3,7 +3,7 @@ cpf= input("Digite p cpf:")
 primeiro_digito=0
 segundo_digit0=0
 soma=0 
-soma2=0
+#soma2=0
  
  #calculando primeiro cpf 
 for cont in range (10,1,-1):
@@ -15,15 +15,17 @@ if soma %11<2:
 else:
     primeiro_digito=11-(soma%11)
 
+soma=0
 #calculando o segundo digito 
 for cont in range (11,1,-1):
     numero=cpf[11-cont]
-    soma2+=int(numero)*cont
+    soma+=int(numero)*cont
 
-if soma2%11<2:
+
+if soma%11<2:
     segundo_digit0=0
 else:
-    segundo_digit0=11-(soma2%11)
+    segundo_digit0=11-(soma%11)
 
 #verificando se o cpf é válido
 if cpf[-2]==str(primeiro_digito) and cpf[-1]==str(segundo_digit0):
